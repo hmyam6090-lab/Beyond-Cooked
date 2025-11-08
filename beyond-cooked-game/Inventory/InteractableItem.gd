bff2cc25f25f359c7818a1befcdcb45545f766b2
Yam <hmyam6090@gmail.com>
2025-11-07 22:43:33 -0500

    New mechanics implemented
    I added an inventory system, with 4 slots like lethal company.
    
    The player can now "pick up" items nearby to them that are highlighted and glow with "E"

---
+++ beyond-cooked-game/Inventory/InteractableItem.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name InteractableItem
+
+@export var ItemHighlightMesh : MeshInstance3D
+
+func GainFocus():
+	ItemHighlightMesh.visible = true
+	
+func LoseFocus():
+	ItemHighlightMesh.visible = false
--- beyond-cooked-game/Scripts/player.gd
+++ beyond-cooked-game/Scripts/player.gd
@@ -17,11 +17,7 @@ var moveDir: Vector3
 @export var followSpeed = 5.0
 @export var followDistance = 2.5
 @export var maxDistanceFromCamera = 5.0
-@export var dropBelowPlayer = false
-@export var groundRay: RayCast3D # Only needed if dropBelowPlayer is true
 
-@onready var interactRay = $Camera3D/InteractRay
-var heldObject: RigidBody3D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -35,8 +31,6 @@ func _process(delta):
 	if Input.is_action_just_pressed("jump") && is_on_floor(): velocity.y = jumpForce
 	
 func _physics_process(delta):
-	# Holding Physics Objects
-	handle_holding_objects()
 	
 	# Gravity
 	if !is_on_floor(): velocity.y -= gravity * delta
@@ -55,38 +49,3 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * mouseSens.y * 0.01)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-45), deg_to_rad(45))
 		
-func set_held_object(body):
-	if body is RigidBody3D and body.is_in_group("pickable"):
-		heldObject = body
-	
-func drop_held_object():
-	heldObject = null
-	
-func throw_held_object():
-	var obj = heldObject
-	drop_held_object()
-	obj.apply_central_impulse(-camera.global_basis.z * throwForce * 10)
-	
-func handle_holding_objects():
-	# Throwing Objects
-	if Input.is_action_just_pressed("throw"):
-		if heldObject != null: throw_held_object()
-		
-	# Dropping Objects
-	if Input.is_action_just_pressed("interact"):
-		if heldObject != null: drop_held_object()
-		elif interactRay.is_colliding(): set_held_object(interactRay.get_collider())
-		
-	# Object Following
-	if heldObject != null:
-		var targetPos = camera.global_transform.origin + (camera.global_basis * Vector3(0, 0, -followDistance)) # 2.5 units in front of camera
-		var objectPos = heldObject.global_transform.origin # Held object position
-		heldObject.linear_velocity = (targetPos - objectPos) * followSpeed # Our desired position
-		
-		# Drop the object if it's too far away from the camera
-		if heldObject.global_position.distance_to(camera.global_position) > maxDistanceFromCamera:
-			drop_held_object()
-			
-		# Drop the object if the player is standing on it (must enable dropBelowPlayer and set a groundRay/RayCast3D below the player)
-		if dropBelowPlayer && groundRay.is_colliding():
-			if groundRay.get_collider() == heldObject: drop_held_object()
